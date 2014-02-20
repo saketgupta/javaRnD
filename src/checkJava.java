de444b369322e4ff1a8bde6edba8e6397e206b26
saketn302 <saket.n302@gmail.com>
2014-02-20 18:30:15 +0530

    initial commit

---
+++ src/checkJava.java
@@ -0,0 +1,12 @@
+
+
+import com.halosys.ExcelmodileModule;
+public class checkJava {
+	
+	public static void main(String a[])
+	{
+		ExcelmodileModule w=new ExcelmodileModule();
+		w.example();
+	}
+
+}
